adac49550bb507f02871a59065afed290409fd43
Robin Kruppe <robin.kruppe@gmail.com>
2017-08-11 09:51:58 +0200

    Support scatter/gather of pointers

--- lib/Target/Nyuzi/NyuziTargetTransformInfo.cpp
+++ lib/Target/Nyuzi/NyuziTargetTransformInfo.cpp
@@ -11,7 +11,10 @@
 
 namespace {
 bool isLegalScatterGatherType(llvm::Type *Ty) {
-  return Ty->isVectorTy() && Ty->getScalarSizeInBits() == 32;
+  auto EltTy = Ty->getVectorElementType();
+  // We don't have a DataLayout, so we need to explictly recognize pointers.
+  // FIXME are there other types that are 32 bits but not covered by this?
+  return EltTy->getPrimitiveSizeInBits() == 32 || EltTy->isPointerTy();
 }
 }
 
